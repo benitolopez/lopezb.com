d9a1daace7d982927c1727b72af3852a16a854b3
Benito Lopez <hello@lopezb.com>
2025-05-08 17:02:03 +0200

    Created ToC component

---
+++ app/components/posts/toc.tsx
@@ -0,0 +1,36 @@
+type TocItem = {
+  title: string;
+  id: string;
+  items?: TocItem[];
+};
+
+type TocProps = {
+  title: string;
+  items: TocItem[];
+};
+
+export default function Toc({ title, items }: TocProps) {
+  const renderItems = (items: TocItem[], depth = 0) => (
+    <ul className={depth === 0 ? "leading-8" : "text-sm leading-6 ml-4"}>
+      {items.map((item) => (
+        <li key={item.id}>
+          <a href={`#${item.id}`} className="hover:text-black">
+            {item.title}
+          </a>
+          {item.items &&
+            item.items.length > 0 &&
+            renderItems(item.items, depth + 1)}
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <>
+      <div className="mb-8 border-t border-b py-6">
+        <div className="text-lg font-bold text-black">Table of contents</div>
+        <div className="text-gray-500 mt-3">{renderItems(items)}</div>
+      </div>
+    </>
+  );
+}
