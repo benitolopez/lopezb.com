3163c69aed7319f521522f886dc35bc6e06b0688
Benito Lopez <hello@lopezb.com>
2025-05-14 10:39:14 +0200

    Types for newsletter component

---
+++ lib/forms.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const NewsletterFormSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+});
+
+export type NewsletterFormState =
+  | {
+      errors?: {
+        email?: string[];
+      };
+      values?: {
+        email?: string;
+      };
+      response?: {
+        message?: string;
+        success?: boolean;
+      };
+    }
+  | undefined;
