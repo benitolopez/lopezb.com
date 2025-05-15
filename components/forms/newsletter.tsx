afb46e943d359779a888654c45e65668bd79f548
Benito Lopez <hello@lopezb.com>
2025-05-15 23:13:36 +0200

    Style fixes for newsletter component

--- components/forms/newsletter.tsx
+++ components/forms/newsletter.tsx
@@ -12,7 +12,7 @@ export default function NewsletterForm() {
 
   return (
     <>
-      <div className="bg-black mt-12 mb-5 p-12 rounded-md">
+      <div className="bg-black mt-12 mb-5 p-6 md:p-12 rounded-md">
         <h2 className="leading-7 text-white mb-5 font-bold text-xl">
           Stay updated on WordPress deep dives
         </h2>
@@ -28,23 +28,25 @@ export default function NewsletterForm() {
               {state.errors.email.join(", ")}
             </span>
           )}
-          <input
-            type="text"
-            name="email"
-            id="email"
-            required
-            className="block w-full py-2 px-5 text-gray-800 placeholder:text-gray-400 rounded-md bg-white focus:outline-gray-400 outline-offset-0"
-            placeholder="Enter your email address"
-            defaultValue={state?.values?.email || ""}
-            aria-invalid={state?.errors?.email ? "true" : "false"}
-          />
+          <div className="relative">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              required
+              className="block w-full py-2 px-5 text-gray-800 placeholder:text-gray-400 rounded-md bg-white focus:outline-gray-400 outline-offset-0 h-[60px]"
+              placeholder="Enter your email address"
+              defaultValue={state?.values?.email || ""}
+              aria-invalid={state?.errors?.email ? "true" : "false"}
+            />
 
-          <button
-            type="submit"
-            className="mt-4 w-full py-3 px-5 rounded-md hover:bg-white hover:text-black focus:outline-none text-left bg-blue-500 text-white"
-          >
-            Subscribe
-          </button>
+            <button
+              type="submit"
+              className="py-3 px-5 rounded-md bg-black focus:outline-none text-left hover:bg-blue-500 text-white top-[3px] right-[3px] absolute h-[54px]"
+            >
+              Subscribe
+            </button>
+          </div>
           {state?.response?.message && (
             <span className="mt-4 block text-white text-sm">
               {state.response?.message}
