831c77c50235bf718f8a084375e8e397aafe6a22
Benito Lopez <hello@lopezb.com>
2025-05-18 19:01:36 +0200

    Add loader to subscribe button

--- components/forms/newsletter.tsx
+++ components/forms/newsletter.tsx
@@ -36,9 +36,38 @@ export default function NewsletterForm() {
             />
             <button
               type="submit"
-              className="py-3 px-5 rounded-md hover:bg-black focus:outline-none text-left bg-blue-500 text-white top-[5px] right-[5px] absolute h-[50px]"
+              disabled={pending}
+              className={`py-3 px-5 rounded-md hover:bg-black focus:outline-none text-left bg-blue-500 text-white top-[5px] right-[5px] absolute h-[50px] flex items-center justify-center ${
+                pending ? "cursor-not-allowed hover:bg-blue-500" : ""
+              }`}
             >
-              Subscribe
+              {pending ? (
+                <>
+                  <svg
+                    className="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                  <span>Processing...</span>
+                </>
+              ) : (
+                "Subscribe"
+              )}
             </button>
           </div>
           {state?.errors?.email && (
