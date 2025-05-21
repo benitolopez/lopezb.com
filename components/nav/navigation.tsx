9bc15575cd9fb8e269464e3a21594fc6aa203b31
Benito Lopez <hello@lopezb.com>
2025-05-21 23:16:04 +0200

    New style for small devices

--- components/nav/navigation.tsx
+++ components/nav/navigation.tsx
@@ -4,8 +4,12 @@ import NavigationLink from "@/components/nav/navigation-link";
 export default function Navigation() {
   return (
     <header className="border-b py-4">
-      <div className="container mx-auto px-12 max-w-3xl flex justify-between items-center">
-        <ul className="flex">
+      <div className="container mx-auto px-12 max-w-3xl sm:flex justify-between items-center">
+        <span className="order-2 text-sm font-medium text-black border-b block pt-2 pb-4 mb-4 sm:p-0 sm:m-0 sm:border-0">
+          Benito Lopez
+        </span>
+
+        <ul className="flex order-1 flex-wrap">
           {MENU_LINKS.map((link) => {
             return (
               <li key={link.id}>
@@ -18,8 +22,6 @@ export default function Navigation() {
             );
           })}
         </ul>
-
-        <span className="text-sm font-medium text-black">Benito Lopez</span>
       </div>
     </header>
   );
