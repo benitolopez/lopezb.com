1602719436184c6f4c93f1b83c2f6b82ae08f2c0
Benito Lopez <hello@lopezb.com>
2025-06-01 23:56:25 +0200

    Add link to home

--- components/nav/navigation.tsx
+++ components/nav/navigation.tsx
@@ -1,13 +1,17 @@
 import { MENU_LINKS } from "@/lib/constants";
 import NavigationLink from "@/components/nav/navigation-link";
+import Link from "next/link";
 
 export default function Navigation() {
   return (
     <header className="border-b py-4">
       <div className="container mx-auto px-12 max-w-3xl sm:flex justify-between items-center">
-        <span className="order-2 text-sm font-medium text-black border-b block pt-2 pb-4 mb-4 sm:p-0 sm:m-0 sm:border-0">
-          Benito Lopez
-        </span>
+        <Link
+          href="/"
+          className="order-2 border-b block pt-2 pb-4 mb-4 sm:p-0 sm:m-0 sm:border-0"
+        >
+          <span className="text-sm font-medium text-black">Benito Lopez</span>
+        </Link>
 
         <ul className="flex order-1 flex-wrap">
           {MENU_LINKS.map((link) => {
