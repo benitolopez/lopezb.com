2147d1ad05c4080219d6743b3dd77d77c4ad0fb0
Benito Lopez <hello@lopezb.com>
2025-07-24 11:18:32 +0200

    Fix post title

--- components/posts/posts.tsx
+++ components/posts/posts.tsx
@@ -9,7 +9,9 @@ export default function Posts({ posts }: { posts: Post[] }) {
       {posts.map(({ slug, title, date }) => (
         <div className="mb-5" key={slug}>
           <h2 className="title leading-7 text-black mb-2 font-bold text-xl">
-            <Link href={`/${BLOG_SLUG}/${slug}`}>{title}</Link>
+            <Link href={`/${BLOG_SLUG}/${slug}`}>
+              {title.replace(" - Benito Lopez", "")}
+            </Link>
           </h2>
           <span className="date text-gray-500">
             {format(parseISO(date), "MMMM d, yyyy")}
