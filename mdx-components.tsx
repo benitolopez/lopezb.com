a1d936f0a9170c4191e2cf6ca182ace5afaf0528
Benito Lopez <hello@lopezb.com>
2025-12-02 09:45:14 +0100

    Minor CSS fix

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -46,7 +46,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       <p className="leading-7 text-gray-800 mb-5">{children}</p>
     ),
     ul: ({ children }) => (
-      <ul className="leading-7 text-gray-800 mb-5 pl-3 list-disc marker:text-gray-300">
+      <ul className="leading-7 text-gray-800 mb-5 pl-3 ml-3 list-disc marker:text-gray-300">
         {children}
       </ul>
     ),
