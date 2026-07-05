8f64742782a6c6b53c5a6a40a4df2640b0f6e0cf
Benito Lopez <hello@lopezb.com>
2026-07-05 12:37:27 +0200

    Reduce site width

--- components/nav/navigation.tsx
+++ components/nav/navigation.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <header className="border-b py-4">
-      <div className="container mx-auto px-6 md:px-8 lg:px-12 max-w-[45rem] sm:flex justify-between items-center">
+      <div className="container mx-auto px-6 md:px-8 lg:px-12 max-w-[40rem] sm:flex justify-between items-center">
         <Link
           href="/"
           className="order-2 border-b block pt-2 pb-4 mb-4 sm:p-0 sm:m-0 sm:border-0"
